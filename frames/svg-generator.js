d619571f306d492fb8774dad8eb3e7aaad48176f
Pierre-Emmanuel Lévesque <pierre.e.levesque@gmail.com>
2021-01-10 01:15:46 -0500

    refactored

--- frames/svg-generator.js
+++ frames/svg-generator.js
@@ -7,5 +7,46 @@ const colorsFile = fs.readFileSync('angklung-colors.txt', 'utf8')
 const frames = lib.extractFrames(framesFile)
 const colors = lib.extractColors(colorsFile)
 
-console.log(frames.length)
-console.log(colors.length)
+// variables
+const cw = 1600 // canvas width
+const ch = 900  // canvas height
+const rw = 120  // rectangle width
+const rh = 60   // rectangle height
+const rsw = 2   // rectangle spacer width
+const rsh = 2   // rectangle spacer height
+
+function renderFrameToSVG(frame) {
+  let svg = ''
+  const pw = (cw - ((frame[0].length * rw) + ((frame[0].length - 1) * rsw))) / 2 // padding width
+  const ph = (ch - ((frame.length * rh) + ((frame.length - 1) * rsh))) / 2 // padding height
+  // The pw can change, but not the pw
+  // color system is wrong, it should work by line
+  let y = ph
+  frame.forEach((block, j) => {
+    let x = pw
+    for (let i = 0; i < block.length; i++) {
+      if (block.charAt(i) !== '.') {
+        const c = colors[j]
+        svg += `  <rect x="${x}" y="${y}" width="${rw}" height="${rh}" fill="#${c}" />\n`
+      }
+      x += rw + rsw
+    }
+    y += rh + rsh
+  })
+  return svg
+}
+
+function createSVG(body) {
+  let svg = ''
+  svg += '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n'
+  svg += `<svg width="${cw}" height="${ch}" style="background-color:#000000" xmlns="http://www.w3.org/2000/svg">\n`
+  svg += body
+  svg += '</svg>'
+  return svg
+}
+
+const svg = createSVG(renderFrameToSVG(frames[25]))
+
+fs.writeFile('test.svg', svg, (err) => {
+  if (err) throw err
+});
